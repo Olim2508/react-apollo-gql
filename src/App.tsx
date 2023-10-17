af55b1981bddd2cb00cfb8fdc9cc1948c3ab7b6d
Olim2508 <rahmatovolim3@gmail.com>
2023-10-17 12:41:20 +0500

    added grapql and tailwind packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CapsuleList from "./components/CapsuleList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-blue-500 p-4 text-white">This is a Tailwind CSS component</div>
+      <CapsuleList/>
     </div>
   );
 }
